3228f87f95e34b5b73e00444e4ff5b27079cd063
jarlekramer <jarlekramer@gmail.com>
2021-07-14 07:29:57 +0200

    Update

---
+++ OpenFOAM case folders/Experimental validation/Calm water resistance/DTMB5415/Fr0.45/mesh.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+surfaceFeatureExtract
+blockMesh
+mv system/decomposeParDict system/decomposeParDict.sim
+mv system/decomposeParDict.mesh system/decomposeParDict
+decomposePar
+mpirun -np 28 snappyHexMesh -overwrite -parallel
+reconstructParMesh -constant
+rm -fr processor*
+mv system/decomposeParDict system/decomposeParDict.mesh
+mv system/decomposeParDict.sim system/decomposeParDict
+renumberMesh -overwrite
+mv system/fvOptions.sim system/fvOptions
